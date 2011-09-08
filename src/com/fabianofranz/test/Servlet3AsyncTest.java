9b5c12dd9285d65cb85182b1c03a212f7d54d754
Fabiano Franz <contact@fabianofranz.com>
2011-09-08 16:37:10 -0300

    Integrated HTML 5 Boilerplate, minor adjustments.

--- src/com/fabianofranz/test/Servlet3AsyncTest.java
+++ src/com/fabianofranz/test/Servlet3AsyncTest.java
@@ -1,11 +1,14 @@
-package foo;
+package com.fabianofranz.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -19,110 +22,108 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/test2", asyncSupported=true, loadOnStartup=0)
-public class Test extends HttpServlet {
-	
+/**
+ * Simple example to test the new Servlet 3 async support in a web app without web.xml descriptor.
+ * @author fabianofranz
+ */
+@WebServlet(name = "Servlet3AsyncTest", value = "/async", asyncSupported = true)
+public class Servlet3AsyncTest extends HttpServlet {
+
 	private Queue<String> messages = new ConcurrentLinkedQueue<String>();
 	private final Executor executor = Executors.newFixedThreadPool(10);
 	private List<AsyncContext> ctxs = new ArrayList<AsyncContext>();
-
-	/**
-	 * Will open an async connection.
-	 */
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+
 		res.setContentType("text/plain");
         res.setCharacterEncoding("utf-8");
         res.setHeader("Access-Control-Allow-Origin", "*");
         
         PrintWriter writer = res.getWriter();
-		writer.print('2');
-		writer.print(';');
-		writer.print("Hi");
-		writer.print(';');
+		writer.print("2;Hi;");
         writer.flush();
 
 		final AsyncContext ctx = req.startAsync();
-		
+
 		ctx.addListener(new AsyncListener() {
-			
+
 			@Override
 			public void onStartAsync(AsyncEvent event) throws IOException { }
-			
+
 			@Override
 			public void onTimeout(AsyncEvent event) throws IOException { 
 				ctxs.remove(ctx);
 			}
-			
+
 			@Override
 			public void onError(AsyncEvent event) throws IOException { 
 				ctxs.remove(ctx);
 			}
-			
+
 			@Override
 			public void onComplete(AsyncEvent event) throws IOException { 
 				ctxs.remove(ctx);
 			}
-			
+
 		});
-		
+
 		ctxs.add(ctx);
-		
+
 	}
-	
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/plain");
         res.setCharacterEncoding("utf-8");
-		messages.add("MSG " + System.currentTimeMillis());
+		messages.add(createRandomMessage());
 	}
-	
+
 	@Override
 	public void init() throws ServletException {
-		
+
 		super.init();
-		
+
 		// produce random messages
-		
+
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				
+
 				while(true) {
-					
-					messages.add("MSG " + System.currentTimeMillis());
-					
+
+					messages.add(createRandomMessage());
+
 					try {
 						Thread.sleep(new Random().nextInt(5) * 1000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
-				
+
 			}
-			
+
 		}).start();
-		
+
 		// print messages to all users
-		
+
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				
+
 				while(true) {
-					
+
 					if (! messages.isEmpty()) {
-						
+
 						final String message = messages.poll();
-						
+
 						executor.execute(new Runnable() {
 							public void run() {
-								
+
 								for (AsyncContext ctx : ctxs) {
-									
+
 									try {
-										
+
 										PrintWriter writer = ctx.getResponse().getWriter(); 
 										writer.print(message.length());
 										writer.print(';');
@@ -133,19 +134,23 @@ public class Test extends HttpServlet {
 									} catch (IOException e) {
 										e.printStackTrace();
 									}
-									
+
 								}
-								
+
 							};
 						});
 					}
-					
+
 				}
-				
+
 			}
-			
+
 		}).start();
-		
+
+	}
+	
+	protected String createRandomMessage() {
+		return DateFormat.getTimeInstance().format(Calendar.getInstance().getTime()) + ' ' + UUID.randomUUID().toString();
 	}
 	
 }
